d28a73c2dc935937fc5d3c2c0b1a284781789c7b
Kittithep <kittithep910@gmail.com>
2025-11-03 16:59:34 +0000

    add 05

---
+++ PRACTICAL_05/raylib_starter/raylib/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/project05/GAMEPLAY_PROGRAMMING_I_2025/PRACTICAL_05/raylib_starter/raylib
+# Build directory: C:/project05/GAMEPLAY_PROGRAMMING_I_2025/PRACTICAL_05/raylib_starter/raylib/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("raylib")
